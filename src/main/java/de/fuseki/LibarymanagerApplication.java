04096e4236b2941bbdd25bc2f9d3e8b334b5c0d8
Ykurt <ykurt@fuseki.com>
2024-03-19 08:47:17 +0100

    Created project and added the database.

---
+++ src/main/java/de/fuseki/LibarymanagerApplication.java
@@ -0,0 +1,13 @@
+package de.fuseki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibarymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fuseki/yusuf/libarymanager/LibarymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fuseki.yusuf.libarymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibarymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
