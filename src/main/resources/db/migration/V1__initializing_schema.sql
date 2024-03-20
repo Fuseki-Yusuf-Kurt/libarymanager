8d679e6b798892eb531c80a0ff260e5e002cbe0a
Ykurt <ykurt@fuseki.com>
2024-03-20 16:56:16 +0100

    addes flyway and the new database

---
+++ src/main/resources/db/migration/V1__initializing_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "person"(
+     "id" INTEGER,
+    "name" TEXT NOT NULL ,
+    "surname" TEXT NOT NULL ,
+    "person_type" TEXT NOT NULL ,
+    "email" TEXT NOT NULL UNIQUE ,
+    "address" TEXT NOT NULL ,
+    "birth_date" TEXT NOT NULL,
+     PRIMARY KEY("id")
+);
---
+++ src/test/java/de/fuseki/service/PersonServiceTest.java
@@ -0,0 +1,24 @@
+package de.fuseki.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+    @Test
+    void getAllPersons() {
+    }
+
+    @Test
+    void deletePerson() {
+    }
+
+    @Test
+    void addNewPerson() {
+    }
+
+    @Test
+    void updatePerson() {
+    }
+}
