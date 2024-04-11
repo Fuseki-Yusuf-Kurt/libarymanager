4e9dda8fc762e479a01b75fea0ae5b0b13140b10
Ykurt <ykurt@fuseki.com>
2024-04-11 16:23:55 +0200

    Added Book Table

---
+++ src/main/resources/db/migration/V2__initializing_Book.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "book"
+(
+    "id"           INTEGER,
+    "title"        TEXT NOT NULL,
+    "author"       TEXT,
+    "genre"        TEXT,
+    "release_date" TEXT,
+    PRIMARY KEY ("id")
+);
