bb1bc3953b5f6addff92f128548a67f77ae06e39
Yusuf Kurt <ykurt@fuseki.com>
2024-07-05 15:28:30 +0200

    added repository

--- src/main/java/de/fuseki/repository/OrderRepository.java
+++ src/main/java/de/fuseki/repository/OrderRepository.java
@@ -1,2 +1,9 @@
-package de.fuseki.repository;public class LoanRepository {
+package de.fuseki.repository;
+
+import de.fuseki.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
