f0c5f54857e3f2fc3f26f200edd02871f190edc8
Yusuf Kurt <ykurt@fuseki.com>
2024-07-05 15:19:08 +0200

    Controller for order

--- src/main/java/de/fuseki/controller/OrderController.java
+++ src/main/java/de/fuseki/controller/OrderController.java
@@ -1,2 +1,24 @@
-package de.fuseki.controller;public class OrderController {
+package de.fuseki.controller;
+
+import de.fuseki.dtos.CreateOrderDto;
+import de.fuseki.dtos.OrderDto;
+import de.fuseki.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    @PostMapping("/order")
+    public ResponseEntity addOrder(@RequestBody CreateOrderDto createOrderDto) {
+        orderService.addOrder(createOrderDto);
+        return ResponseEntity.ok().build();
+    }
 }
