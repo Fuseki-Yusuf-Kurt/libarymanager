867032c5c2bf0c9b16434a8a717ff0750cba574c
Yusuf Kurt <ykurt@fuseki.com>
2024-07-12 10:29:49 +0200

    added Reservation Repository and Controller

---
+++ src/main/java/de/fuseki/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package de.fuseki.repository;
+
+import de.fuseki.entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+}
